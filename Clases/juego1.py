57cc56668746396a357ea01d4c8655815baf8574
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-02-28 10:04:38 -0500

    correcciones

--- Clases/juego1.py
+++ Clases/juego1.py
@@ -12,7 +12,7 @@ PREGUNTAR_NUMERO = '''
 '''
 PREGUNTA_FALLIDA = 'Ahhhhh fallaste...Ingresa otro número : '
 MENSAJE_GANASTE = '¡¡¡Felicidades,ganaste!!!'
-MENSAJE_PERDISTE = 'Perdiste D: . Vuelve a intentarlo :'
+MENSAJE_PERDISTE = 'Perdiste D: . Vuelve a intentarlo ...'
 #----Entrada al código----#
 numeroOculto = 7
 vidas = 5
@@ -24,8 +24,8 @@ while (numeroOculto != numeroIngresado and vidas > 0) :
     numeroIngresado = int (input (PREGUNTA_FALLIDA))
     vidas -=1
 
-if (vidas >= 0) :
+if (vidas >= 0 and numeroOculto == numeroIngresado) : 
     print (MENSAJE_GANASTE)
     print (vidas)
 else :
-    print (MENSAJE_PERDISTE, 'El número era el ,',numeroOculto)
+    print (MENSAJE_PERDISTE, 'El número era el',numeroOculto)
