9a74a8ee031d81848c4a9f4b9dd2884f1ba9dc62
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-03-10 15:00:36 -0500

    .

---
+++ Práctica/cicloswhilepr.py
@@ -0,0 +1,46 @@
+PREGUNTA_NOMBRE = "Ingresa tu nombre.Por favor : "
+nombre = input (PREGUNTA_NOMBRE)
+print(f"Hola {nombre}.Te saludo. Espero que estés muy bien")
+PREGUNTA_DEACUERDO = '''
+        Si - ¿Te vuelvo a saludar?
+        No - Me despido 
+'''
+decision = input (PREGUNTA_DEACUERDO)
+MENSAJE_SALUDO = f"Hola {nombre}.Te saludo nuevamente"
+MENSAJE_DESPEDIDA = f"Hasta luego, {nombre}.Espero verte de nuevo."
+MENSAJE_FALLASTE = 'Ese no es el número. Sigue intentando...'
+MENSAJE_GANASTE = '¡Bien! Acertaste el número'
+
+while (decision == 'Si') :
+    print(MENSAJE_SALUDO)
+    decision = input(PREGUNTA_DEACUERDO)
+print (MENSAJE_DESPEDIDA)
+
+numero_secreto = 9
+PREGUNTA_NUMERO = 'Ingrese un número entero entre el 1 y el 10. Por favor : '
+numero_escogido = int (input(PREGUNTA_NUMERO))
+while (numero_escogido != numero_secreto) :
+    print (MENSAJE_FALLASTE)
+    numero_escogido = int (input( PREGUNTA_NUMERO))
+print(MENSAJE_GANASTE)
+
+
+
+#----Ejercicios While----#
+#Leer números enteros de teclado, hasta que el usuario ingrese 0. Finalmente,mostrar la sumatoria de todos los número ingresados.
+numero_oculto2 = 0
+#----Mensajes----#
+MENSAJE_SALUDO2= 'Hola. Es un placer interactuar contigo,espero que te encuentres bien'
+MENSAJE_DESPEDIDA2 = 'Hasta luego. Ten un bonito día'
+MENSAJE_FALLASTE2 = '¡Ups! Ese no era el número. Sigue intentando...'
+MENSAJE_GANASTE2 = f'¡Muy bien! El número {numero_oculto2} era el correcto.'
+
+#----Entrada al código----#
+print(MENSAJE_SALUDO2)
+PREGUNTA_NUMERO2 = 'Ingresa un número entero de una cifra. Por favor : '
+numero_escogido2 = int(input(PREGUNTA_NUMERO2))
+
+while (numero_escogido2 != numero_oculto2):
+    print(MENSAJE_FALLASTE2)
+    numero_escogido2= int (input(PREGUNTA_NUMERO2))
+print(MENSAJE_GANASTE2) 
