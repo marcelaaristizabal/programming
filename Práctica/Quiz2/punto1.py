c2f14650479556432fd441a1bb925bc2c621db75
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-03-15 23:12:21 -0500

    taller

--- Práctica/Quiz2/punto1.py
+++ Práctica/Quiz2/punto1.py
@@ -1,34 +1,28 @@
-listaPesos = [20000,30000,4000,2500,1000,7600]
-PREGUNTA_MONEDA ='''
-    C- Mostrar original en pesos colombianos 
-    D- Mostrar en Dólares
-    E- Mostrar en Euros
-'''
-MENSAJE_PESOS = 'Mostrando lista original'
-MENSAJE_DOLARES = 'Mostrando lista en dólares'
-MENSAJE_EURO ='Mostrando lista en euros'
-MENSAJE_ERROR = 'INGRESO NO VÁLIDO'
+listaDolares = [20000,30000,4000,2500,1000,7600]
 
+#Conversión punto 1
 listaEuros = []
-for elemento in listaPesos:
-    conversor = round(elemento*0.00023,2)
-    listaEuros.append(conversor)
-listaDolares = []
 for elemento in listaDolares:
-    conversor = round(elemento*0.00028,2)
-    listaDolares.append(conversor)
+    conversor = round(elemento*0.84)
+    listaEuros.append(conversor)
+listaPesos = []
+for elemento in listaPesos:
+    conversor = round(elemento*3700)
+    listaPesos.append(conversor)
 
-opcion_moneda = input(PREGUNTA_MONEDA)
-if (opcion_moneda == 'C') :
-    print(MENSAJE_PESOS)
-    print (listaPesos)
-elif (opcion_moneda == 'D'):
-    print (MENSAJE_DOLARES)
-    print(listaDolares)
-elif (opcion_moneda == 'E') :
-    print(MENSAJE_EURO)
-    print (listaEuros)
-else :
-    print(MENSAJE_ERROR)
-
-print('Feliz día')
+opcion_escogida= int(input(PREGUNTA_NUMERO))
+while (opcion_escogida != 5) :
+    #----------------Opción1----------------#
+    if (opcion_escogida == 1) :
+        opcion_moneda = input(PREGUNTA_MONEDA)
+        if (opcion_moneda == 'C') :
+            print(MENSAJE_PESOS)
+            print (listaPesos)
+        elif (opcion_moneda == 'D'):
+            print (MENSAJE_DOLARES)
+            print(listaDolares)
+        elif (opcion_moneda == 'E') :
+            print(MENSAJE_EURO)
+            print (listaEuros)
+        else :
+            print(MENSAJE_ERROR)
