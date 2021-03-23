344c9267b2d9c5445e86cbeccd435967981e1d2d
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-03-23 07:22:54 -0500

    importando

--- Clases/Funciones/funciones.py
+++ Clases/Funciones/funciones.py
@@ -1,38 +1,12 @@
-guardar = print ('Hola')
-print (guardar)
-
-guardar = round(14.2534897,2)
-print(guardar)
-
-def linedesing (cantidad = 10 , simbolo= '#' ):
-    print (simbolo * cantidad)
-    return None 
-
-linedesing(30, '#')
-linedesing(10, '*')
-linedesing(100,'')
-
-
-#----------------Mostrar la lista-----------------#
-def mostrarlista (listaEntrada= []):
-    for elemento in listaEntrada:
-        print (elemento)
-    return None
-lista = [213,32,23123,321,321,233,1232,23]
-lista2= [564654,645,64543,2565,547,57865]
-linedesing(30,'☺')
-mostrarlista(lista)
-linedesing(3,'(⊃｡•́‿•̀｡)⊃' )
-mostrarlista(lista)
 #------------------Sumar 2 números-----------------------#
 def sumar (a = 0 ,b = 0):
+    '''
+        devuelve la suma de dos números A y B
+        por defecto a vale 0 al igual que b
+    '''
     suma = a + b
     return suma
-linedesing(30,'⌘')
-resultado = sumar ()
-print (resultado)
-print (sumar(12,14))
-round (12.234897,2)
+
 #------------------Restar 2 números-----------------------#
 def restar (a = 0 ,b = 0):
     resta= a - b
@@ -45,7 +19,6 @@ def multiplicar (a = 0 ,b = 0):
 def dividir (a = 0 ,b = 1):
     dividi = a / b
     return dividi
-
 #------------------Elevar a una potencia un número------------------#
 PREGUNTA_EXPONENTE= 'Ingresa un número que quieras calcular (exponente) dado una base que elijas : '
 PREGUNTA_BASE = 'Ingresa una base que quieras calcular : '
@@ -54,15 +27,7 @@ numero_exponente = int (input(PREGUNTA_EXPONENTE))
 def potenciar (base = 0 ,exponente = 1):
     potencia = base ** exponente
     return potencia
-
 print (potenciar (numero_base, numero_exponente))
 #-----------------Funciones que dependen de otras----------------------#
 def calcular (operacion, numeroA,numeroB):
     print (operacion(numeroA,numeroB))
-
-print(restar(83,87))
-print(multiplicar(83,87))
-print(dividir(83,87))
-print(dividir())
-
-calcular (multiplicar, 63,67)
---
+++ Clases/Funciones/usandoFunciones.py
@@ -0,0 +1,4 @@
+import funciones as fn 
+import random as rd
+print (fn.sumar (2,4))
+print (rd.randint(2,4))
