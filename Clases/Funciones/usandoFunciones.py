62c0a41bb82ed0abe712fac04d95b3d3f517a732
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-03-23 07:27:31 -0500

    importando

--- Clases/Funciones/usandoFunciones.py
+++ Clases/Funciones/usandoFunciones.py
@@ -1,4 +1,10 @@
 import funciones as fn 
 import random as rd
+def sumar (a,b):
+    return a+b
+print(sumar(2,6))
 print (fn.sumar (2,4))
 print (rd.randint(2,4))
+print (fn.sumar(fn.sumar,2,4))
+
+print(rd.randint(2,6))
