63037211d83a719e380c8be89a32e066207156e8
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-03-24 18:31:24 -0500

    documentando funciones

--- Clases/Funciones/funciones.py
+++ Clases/Funciones/funciones.py
@@ -1,22 +1,34 @@
 #------------------Sumar 2 números-----------------------#
 def sumar (a = 0 ,b = 0):
     '''
-        devuelve la suma de dos números A y B
-        por defecto a vale 0 al igual que b
+        Devuelve la suma de dos números A y B.
+        Por defecto, A vale 0 al igual que B.
     '''
     suma = a + b
     return suma
 
 #------------------Restar 2 números-----------------------#
 def restar (a = 0 ,b = 0):
+    '''
+        Devuelve la resta de dos números A y B.
+        Por defecto, A vale 0 al igual que B
+    '''
     resta= a - b
     return resta
 #------------------Multiplicar 2 números-----------------------#
 def multiplicar (a = 0 ,b = 0):
+    '''
+        Devuelve la multiplicación de dos números A y B.
+        Por defecto, A vale 0 al igual que B.
+    '''
     multiplica = a * b
     return multiplica
 #------------------Dividir 2 números-----------------------#
 def dividir (a = 0 ,b = 1):
+    '''
+        Devuelve la división entre dos números A y B.
+        Por defecto, A vale y B vale 1 (B != 0).
+    '''
     dividi = a / b
     return dividi
 #------------------Elevar a una potencia un número------------------#
@@ -25,6 +37,11 @@ PREGUNTA_BASE = 'Ingresa una base que quieras calcular : '
 numero_base = int (input(PREGUNTA_BASE))
 numero_exponente = int (input(PREGUNTA_EXPONENTE))
 def potenciar (base = 0 ,exponente = 1):
+    '''
+        Devuelve la potenciación de una base A y un exponente B.
+        Se puede ingresar la base y el exponente. 
+        Pero por defecto, A vale 0 y B vale 1
+    '''
     potencia = base ** exponente
     return potencia
 print (potenciar (numero_base, numero_exponente))
