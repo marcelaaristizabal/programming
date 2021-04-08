8c068be729e22cf31fde5de4137add1ac04de59d
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-04-08 06:15:15 -0500

    organizando

--- Clases/Funciones/quiz2.py
+++ Clases/Funciones/quiz2.py
@@ -6,60 +6,52 @@ PREGUNTA_NUMERO = ''' Ingrese alguna de estas opciones
     3. Mostrar topes
     4. Salir
 '''
-PREGUNTA_MONEDA ='''
+PREGUNTA_TEMPERATURA ='''
     K- Mostrar Kelvin
     F- Mostrar  Fahrenheit
     C- Mostrar Celsius
 '''
 
+#-------Mensajes-------#
 MENSAJE_CELSIUS = 'Mostrando lista original. No es necesaria la conversión.'
 MENSAJE_FAHRENHEIT = 'Mostrando lista en Fahrenheit'
 MENSAJE_KELVIN ='Mostrando lista en Kelvin'
 MENSAJE_DESPEDIDA = 'Que tengas un feliz día'
+#-------Error-------#
+MENSAJE_ERROR_ENTRADA:'Valor ingresado no válido.'
 
 temperaturaCorporal = [36,37,38,35,36,38,37.5,38.2,41,37.4,38.6,39.1,40.3,33]
+
+temperaturaCorporalFahrenheit = ct.conversionTemperatura(temperaturaCorporal, 'F')
+temperaturaCorporalKelvin = ct.conversionTemperatura(temperaturaCorporal, 'K')
+clasificaciónTemperaturas = ct.clasificarTemperaturas(temperaturaCorporal)
+
 opcion_escogida= int(input(PREGUNTA_NUMERO))
 while (opcion_escogida != 4 ):
     #----------------Opción1----------------#
     if (opcion_escogida == 1) :
-        opcion_temperatura = input(PREGUNTA_MONEDA)
+        opcion_temperatura = input(PREGUNTA_TEMPERATURA)
         if (opcion_temperatura== 'C') :
             print(MENSAJE_CELSIUS)
-            print (temperaturaCorporal)
+            fn.mostrarLista(temperaturaCorporal)
         elif (opcion_temperatura == 'F'):
             print (MENSAJE_FAHRENHEIT)
-            print(temperaturaCorporalFahrenheit)
+            fn.mostrarLista(temperaturaCorporalFahrenheit)
         elif (opcion_temperatura == 'K') :
             print(MENSAJE_KELVIN)
-            print (temperaturaCorporalKelvin)
+            fn.mostrarLista(temperaturaCorporalKelvin)
         else :
-            print(MENSAJE_ERROR)
+            print(MENSAJE_ERROR_ENTRADA)
     #------------------Opción 2---------------#
     elif (opcion_escogida == 2 ) :
-        valor_ingresado = float (input(PREGUNTAR_NUMERO))
-        listaPesos.append (valor_ingresado)
-        print (listaPesos)
+        print('Mostrando clasificación')
+        print('ºC','\t','Clasificación')
+        fn.mostrar2Lista(temperaturaCorporal, clasificacionTemperaturas)
     #-------------------Opción 3--------------#
     elif (opcion_escogida == 3) :
-        print (MENSAJE_MAYOR, max(listaPesos))
-        print (MENSAJE_MENOR, min(listaPesos))
-        print (MENSAJE_PROMEDIO, sum(listaPesos)/len (listaPesos))
-    #-------------------Opción 4 ------------#
-    elif (opcion_escogida == 4) :
-        posicion = int (input(PREGUNTA_BORRAR_COORDENADA)) - 1
-        print (listaPesos)
-        listaPesos.pop (posicion)
-        print (listaPesos)
+        ct.mostrarTopes(temperaturaCorporal)
     #------------------Opción no válida------------#
     else :
-        print (MENSAJE_ERROR)
+        print (MENSAJE_ERROR_ENTRADA)
     opcion_escogida =int(input(PREGUNTA_NUMERO))
-temperaturaCorporalFahrenheit = ct.conversionTemperatura(temperaturaCorporal, 'F')
-temperaturaCorporalKelvin = ct.conversionTemperatura(temperaturaCorporal, 'K')
-clasificaciónTemperaturas = ct.clasificarTemperaturas(temperaturaCorporal)
-fn.linedesing(60, "")
-print (temperaturaCorporalFahrenheit)
-fn.linedesing(60, "#")
-print (temperaturaCorporalKelvin)
-fn.linedesing(60, "#")
-print(clasificaciónTemperaturas)
+print(MENSAJE_DESPEDIDA)
