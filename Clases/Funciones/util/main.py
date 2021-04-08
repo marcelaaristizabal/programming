ec6f734bd69f3e0974be9b9d230f2b1d4fe29662
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-04-07 22:26:45 -0500

    .

--- Clases/Funciones/util/main.py
+++ Clases/Funciones/util/main.py
@@ -1,23 +1,4 @@
-import operaciones_matematicas as opm 
-import operaciones_strings as ops
-#--------Mensajes--------#
-MENSAJE_OPERACION='{} las dos entradas'
-ops.linedesing(12)
-numeroA = int (input('Ingrese un número A : '))
-numeroB = int (input('Ingrese un número B : '))
-ops.linedesing(12)
-print(MENSAJE_OPERACION.format('Sumando'))
-opm.calcular (opm.sumar,numeroA,numeroB)
-ops.linedesing(12)
-ops.linedesing(12)
-print(MENSAJE_OPERACION.format('Restando'))
-opm.calcular (opm.restar,numeroA,numeroB)
-ops.linedesing(12)
-ops.linedesing(12)
-print(MENSAJE_OPERACION.format('Multiplicando'))
-opm.calcular (opm.multiplicar,numeroA,numeroB)
-ops.linedesing(12)
-ops.linedesing(12)
-print(MENSAJE_OPERACION.format('Dividiendo'))
-opm.calcular (opm.dividir,numeroA,numeroB)
-ops.linedesing(12)
+import operaciones_matematicas 
+import operaciones_strings
+
+operaciones_strings.linedesing(4)
