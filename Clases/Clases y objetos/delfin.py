464a2e36c1fcab90c0ca80f951cfc6223575cdbd
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2021-04-19 22:45:27 -0500

    ...

--- Clases/Clases y objetos/delfin.py
+++ Clases/Clases y objetos/delfin.py
@@ -1,3 +1,14 @@
+tamañonacidoDelfin = 'al nacer, medía alrededor de 80-100 cm.'
+denticionDelfin = '41 y 54 pares de dientes en ambas hemimandíbulas, pudiendo presentar, uno o dos pares más, en la superior.'
+pigmentacionDelfin= '''El color gris oscuro predomina en la superficie dorsal, desde la frente hasta la aleta dorsal donde forma un pico invertido,
+trazándose un dibujo en el lateral que recuerda a un “reloj de arena”, cuyo fragmento anterior es de color amarillo,contrastando con el color oscuro de la parte posterior de mismo,
+que se extiende hasta el pedúnculo caudal. Ventralmente es blanco
+'''
+datosCuriosos ='''Las aletas dorsales y pectorales son oscuras, aunque en adultos la dorsal presenta un tono gris en la parte central. 
+Destaca una línea que parte de la zona genital hacia delante atravesando el parche torácico amarillo.
+Así como una banda negra que une aletas pectorales con el maxilar inferior. Los juveniles tienen una coloración más clara.
+Los machos adultos tienen una quilla en el área genital, marcada con la edad.
+'''
 MENSAJE_CONTEXTO ='Hola,en el día de hoy te mostraré en pantalla algunos datos curiosos de los delfines (De acuerdo a cada especie)'
 PREGUNTAGENERO = ''' ¿De qué género de la especie Delphinus Delphis quisieras conocer ? :
                     1 - Hembra
@@ -32,13 +43,14 @@ class Animal ():
         elif (genero == 'Macho'):
             nombreEntrada = 'Tómas'
             pesoDelfin = 'El promedio normal del peso de un macho de la especie Delphinus Delphis es de 200 kilogramos.'
-            tamañoDelfin = '''En el PACÍFICO ORIENTAL los machos pueden medir entre 1,7-2,3 m
+            tamañoDelfin = '''en el PACÍFICO ORIENTAL  medir entre 1,7-2,3 m
                         Mientras que en ATLÁNTICO ORIENTAL tienen un tamaño promedio de 2,70m. 
-            '''
+                    '''
             edadEntrada = 'Los machos de esta especie pueden sobrevivir en cautiverio cerca de 40 años.'
         else:
             print('INGRESA UNA OPCIÓN VÁLIDA.')
-    
+        self.denticion = denticionDelfin
+        self.pigmentacion = pigmentacionDelfin
         self.edad = edadEntrada
         self.peso = pesoDelfin
         self.nombre= nombreEntrada
@@ -57,44 +69,34 @@ class Animal ():
                         c- Mar MEDITERRÁNEO
                         d- Mar Negro
         '''
-    def saludar (self,saludo):
+
+    def saludar (self):
         #Los delfines suelen utilizar silbidos únicos propios de cada uno 
         #cuando se reunen con otro grupo a modo de saludo y presentación.
-        print(f'Hola, soy {self.nombre}. Así es como te saludo', saludo)
+        print (f'Hola, soy {self.nombre}. Así es como te saludo Güi, güi...Tototo...')
     
-
     def mostrarAtributos (self):
         print(f''' Mi nombre {self.nombre}. Soy un@ delfín muy feliz.
                     {self.tamaño}
                     {self.edad}  
                     {self.peso}
-        ''')
-    def datosCuriosos (self):
-        print ('''Al nacer, el delfín común oceánico mide alrededor de 80-100 cm.
-            DENTICIÓN : 41 y 54 pares de dientes en ambas hemimandíbulas, pudiendo presentar, uno o dos pares más, en la superior. 
-
-            PIGMENTACIÓN: El color gris oscuro predomina en la superficie dorsal, desde la frente hasta la aleta dorsal,
-            donde forma un pico invertido,trazándose un dibujo en el lateral que recuerda a un “reloj de arena”, cuyo fragmento anterior es de color amarillo,
-            contrastando con el color oscuro de la parte posterior del mismo, que se extiende hasta el pedúnculo caudal. Ventralmente es blanco.
-
-            Las aletas dorsales y pectorales son oscuras, aunque en adultos la dorsal presenta un tono gris en la parte central. 
-            Destaca una línea que parte de la zona genital hacia delante atravesando el parche torácico amarillo.
-            Así como una banda negra que une aletas pectorales con el maxilar inferior. Los juveniles tienen una coloración más clara.
-            Los machos adultos tienen una quilla en el área genital, marcada con la edad.
+                    {tamañoN} 
+                    {self.denticion}
         ''')
 
     def alimentar (self):
         print('La alimentación del delfín común macho o hembra se basa en calamares y peces pequeños que forman bancos, alimentándose de noche y descansando por el día.')
 
-saludo = '''‘Güi, güi...
-            Tototo...’
-'''
+    def rosado (self):
+            self.especie = "Rosado"
+    def salvadorDeVidas (self,vidas):
+        print(f'Güi, güi...Tototo...,soy {self.nombre} y puedo ayudar a salvar vidas de personas {vidas}')
+        print ('Este tipo de delfines puede pesar cerca de 150 kilogramos')
 
 
 delfin = Animal(genero)
-delfin.saludar(saludo)
+delfin.saludar()
 print(delfin.especie)
 delfin.alimentar()
 print(delfin.ubicacion)
 delfin.mostrarAtributos()
-delfin.datosCuriosos()
